757ac916d963189d14c874d32a95bdaedb01a437
fabianbarua <baruafabian@gmail.com>
2024-09-08 00:16:19 -0400

    add react & photo view

---
+++ src/components/Photos.tsx
@@ -0,0 +1,31 @@
+import 'react-photo-view/dist/react-photo-view.css';
+
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+
+export const Photos = () => {
+
+    const images = [
+        {
+            id: 'cap1',
+            url: '/cap1.jpeg'
+        },
+        {
+            id: 'cap2',
+            url: '/cap2.jpeg'
+        },
+        {
+            id: 'cap3',
+            url: '/cap3.jpeg'
+        }
+    ]
+
+    return (
+        <PhotoProvider>
+            {images.map((item, index) => (
+                <PhotoView key={item.id} src={item.url}>
+                    <img className=' w-40 overflow-hidden aspect-square sm:aspect-auto object-cover sm:h-auto sm:w-40 rounded-xl' src={item.url} alt="" />
+                </PhotoView>
+            ))}
+        </PhotoProvider>
+    );
+}
